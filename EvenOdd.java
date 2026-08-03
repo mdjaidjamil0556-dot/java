4e9d6c3951725e79351bde5b1d81c139b23ac02b
mdjaidjamil0556-dot <mdjaidjamil0556@gmail.com>
2026-08-03 17:09:29 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,9 @@
+public class EvenOdd{
+    public static void main(string[]) args){
+        int num = 15;
+        if (num % 2 == 0)
+        System.out.println("Even");
+    else
+        System.out.println("odd")
+    }
+}
